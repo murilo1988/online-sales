1f798b8d2c84896171f5ccffa501bcf4f14d00e1
Murilo Cabreira <murilocabreira@hotmail.com>
2023-05-14 01:07:20 -0300

    feat: container and logo created

--- src/modules/login/screens/LoginScreen.tsx
+++ src/modules/login/screens/LoginScreen.tsx
@@ -1,8 +1,15 @@
-import { BackgroundImage } from "../styles/loginScreens.style";
+import { BackgroundImage, ContainerLogin, LogoImage } from "../styles/loginScreens.style";
+import { LimitedContainer } from './../styles/loginScreens.style';
 function LoginScreen() {
     return (
         <div>
             <BackgroundImage src="./capaLogin.jpg" />
+            <ContainerLogin>
+                <LimitedContainer>
+                    <LogoImage src="./logoLogin.png" />
+                </LimitedContainer>
+            </ContainerLogin>
+
         </div>
     )
 }
--- src/modules/login/styles/loginScreens.style.ts
+++ src/modules/login/styles/loginScreens.style.ts
@@ -7,6 +7,36 @@ export const BackgroundImage = styled.img`
     width: 100%;
     height: 100vh;
     object-fit: cover;
+    z-index: 1;
 
 `;
+export const ContainerLogin = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #ffffff;
+    height: 100vh;
+    width: 100%;
+    max-width: 550px;
+    position: absolute;
+    top: 0;
+    right: 0;
+    z-index: 2;
+   
+
+`
+
+export const LogoImage = styled.img`
+   width: 200px;
+ 
+`;
+export const LimitedContainer = styled.div`
+  border: 1px solid black;
+  display: flex;
+  justify-content: center;
+  width: 85%;
+  max-width: 476px;
+
+`;
+
 
